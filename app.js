f2b8291bff67392d654c1fb3e43634de3d56ffc2
Georgi2024 <anaginasan@gmail.com>
2024-02-12 04:34:59 -0300

    REGIONES-CONTROLADO

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require("node:path");
+//Routers
 
+    app.get('/puna', ( req, res ) => {
+    const pathpuna = path.join(__dirname, "views/puna.html")
 
-app.use(express.static(path.join(__dirname, 'public')));
+    res.sendFile(pathpuna);
+})
+    app.get('/quebrada', ( req, res ) => {
+    const pathquebrada = path.join(__dirname, "views/quebrada.html")
+    
+    res.sendFile(pathquebrada);
+    })
+    app.get('/valles', ( req, res ) => {
+    const pathvalles = path.join(__dirname, "views/valles.html")
+        
+    res.sendFile(pathvalles);
+    })
+    app.get('/yungas', ( req, res ) => {
+    const pathyungas = path.join(__dirname, "views/yungas.html")
+            
+    res.sendFile(pathyungas);
+    })
 
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'home.html')); // 
-});
-
-
-app.listen(3000, function() {
-    console.log('Arrancando Express desde el puerto 3000');
-});
+    app.use(express.static("public"))
+    app.listen( 3000, () => console.log("Server on PORT: http://localhost:3000"))
